c6827c47794209cf213cb1af35c9330e2915ca4a
zoejoblin <zoe@hnry.io>
2024-02-01 16:31:41 +1300

    add form to create a new invoice

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,5 +7,29 @@ class InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
   end
 
+  def new
+    @invoice = Invoice.new
+    @invoice.items.build
+  end
+
+  def create
+
+    @invoice = Invoice.new(invoice_params)
+    
+    # where to get user_id from? doesnt make sense to pass from form 
+    if @invoice.save
+      redirect_to @invoice
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def invoice_params
+      params
+        .require(:invoice)
+        .permit(:invoice_number, :total, :user_id,
+          items_attributes: [:id, :description, :price])
+    end
 
 end
